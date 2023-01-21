97db07d7aa1e06e3e9ea52320aa0c1701a05bf08
Lizzie <leejihye7117@gmail.com>
2023-01-21 16:14:10 +0900

    gh-1 QueryClientProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { GlobalStyle, Wrapper } from "../styles/GlobalStyle";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyle />
-      <Layout>
-        <Wrapper>
-          <Component {...pageProps} />
-        </Wrapper>
-      </Layout>
+      <QueryClientProvider client={queryClient}>
+        <Layout>
+          <Wrapper>
+            <Component {...pageProps} />
+          </Wrapper>
+        </Layout>
+      </QueryClientProvider>
     </>
   );
 }
