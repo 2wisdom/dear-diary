5848835cbce9713c1a6ec06969bc61f41334b632
Lizzie <leejihye7117@gmail.com>
2023-01-22 22:11:08 +0900

    test 삭제

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,8 @@ import styles from "../styles/Home.module.css";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Home() {
-  const { data: version } = useQuery(["initApp"], async () => {
-    return "1.0";
-  });
   return (
     <div>
-      {version}
       <h1>Dear Diary!</h1>
     </div>
   );
