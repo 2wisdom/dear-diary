1ec1b8e5bd8b00bed4bf6f3a7ccdafd92943a54d
Lizzie <leejihye7117@gmail.com>
2023-02-01 22:37:38 +0900

    main book svg animaition

--- styles/IndexStyle.ts
+++ styles/IndexStyle.ts
@@ -1,6 +1,22 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+/* Drawing Style */
+const bookDrawingLength = 6500;
+const pathAni = keyframes`
+0% {stroke-dashoffset: ${bookDrawingLength}}
+100% {stroke-dashoffset: 0}
+`;
 
 export const IndexStyle = styled.div`
   display: flex;
   margin-top: 10rem;
+
+  /* svg path */
+  path {
+    stroke: #78a67e;
+    stroke-width: 3;
+    fill: none;
+    stroke-dasharray: ${bookDrawingLength};
+    animation: ${pathAni} 5s;
+  }
 `;
