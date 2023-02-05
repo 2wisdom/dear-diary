9df628c98bc3b4ef424a942fe7ad28739f252d8d
Lizzie <leejihye7117@gmail.com>
2023-02-05 13:50:53 +0900

    gh-10

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
+let globalWithPrisma = global as typeof globalThis & {
+  prisma: PrismaClient;
+};
+
 let prisma: PrismaClient;
 
 if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
-  let globalWithPrisma = global as typeof globalThis & {
-    prisma: PrismaClient;
-  };
   if (!globalWithPrisma.prisma) {
     globalWithPrisma.prisma = new PrismaClient();
   }
