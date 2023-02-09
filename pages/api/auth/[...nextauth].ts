aa198d710f87f1546de4e77c7a1456416453e61e
Lizzie <leejihye7117@gmail.com>
2023-02-09 23:56:48 +0900

    kakao login 기본 틀 생성

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,77 +1,12 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../lib/prisma";
-
-export default NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      // 서명 양식에 표시할 이름(e.g. "Sign in with...")
-      name: "Credentials",
-      // 'credentials'은 로그인 페이지에서 양식을 생성하는 데 사용됩니다.
-      // credentials 개체에 키를 추가하여 제출할 필드를 지정할 수 있습니다.
-      // e.g. domain, username, password, 2FA token, etc.
-      // HTML 속성을 개체를 통해 <input> 태그에 전달할 수 있습니다.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "username" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        // 제공된 credentials 사용자를 조회하는 로직을 여기에 추가합니다.
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-        if (user) {
-          // 반환된 개체는 JWT의 'user' 속성에 저장됩니다
-          return user;
-        } else {
-          // null을 반환하면 사용자에게 세부 정보를 확인하라는 오류가 표시됩니다.
-          return null;
-
-          // 오류와 함께 이 콜백을 거부할 수도 있으므로 사용자는 오류 메시지를 쿼리 매개 변수로 사용하여 오류 페이지로 전송됩니다.
-        }
-      },
-    }),
-  ],
-});
-
-/*
-import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../lib/prisma";
+import KakaoProvider from "next-auth/providers/kakao";
 
 export const authOptions = {
-  adapter: PrismaAdapter(prisma),
   providers: [
-    CredentialsProvider({
-      // 서명 양식에 표시할 이름(e.g. "Sign in with...")
-      name: "Credentials",
-      // 'credentials'은 로그인 페이지에서 양식을 생성하는 데 사용됩니다.
-      // credentials 개체에 키를 추가하여 제출할 필드를 지정할 수 있습니다.
-      // e.g. domain, username, password, 2FA token, etc.
-      // HTML 속성을 개체를 통해 <input> 태그에 전달할 수 있습니다.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "username" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        // 제공된 credentials 사용자를 조회하는 로직을 여기에 추가합니다.
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
-
-        if (user) {
-          // 반환된 개체는 JWT의 'user' 속성에 저장됩니다
-          return user;
-        } else {
-          // null을 반환하면 사용자에게 세부 정보를 확인하라는 오류가 표시됩니다.
-          return null;
-
-          // 오류와 함께 이 콜백을 거부할 수도 있으므로 사용자는 오류 메시지를 쿼리 매개 변수로 사용하여 오류 페이지로 전송됩니다.
-        }
-      },
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID!,
+      clientSecret: process.env.KAKAO_CLIENT_SECRET!,
     }),
   ],
 };
-
 export default NextAuth(authOptions);
-*/
--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,13 +1,17 @@
 import Image from "next/image";
 import React from "react";
+import LoginBtn from "../components/login-btn";
 
 export default function SignIn() {
   return (
-    <Image
-      src="/images/kakao_login_medium_wide.png"
-      alt="kakao-login"
-      width={300}
-      height={45}
-    />
+    <>
+      <Image
+        src="/images/kakao_login_medium_wide.png"
+        alt="kakao-login"
+        width={300}
+        height={45}
+      />
+      <LoginBtn />
+    </>
   );
 }
