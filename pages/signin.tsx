354fa15e0a9b4d19df70be3e59a47fc13cc9a9ab
Lizzie <leejihye7117@gmail.com>
2023-02-10 02:21:12 +0900

    로그인 페이지 타이틀 추가

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import React from "react";
 import KakaoLogin from "../components/KakaoLogin";
+import { Wrapper } from "../styles/AuthStyle";
 
 export default function SignIn() {
   return (
-    <>
+    <Wrapper>
+      <h3>오늘도 환영해요!</h3>
       <KakaoLogin />
-    </>
+    </Wrapper>
   );
 }
