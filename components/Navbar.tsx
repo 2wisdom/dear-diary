a6accbc48aba47e9393bd35681700e2fc0e7aa32
Lizzie <leejihye7117@gmail.com>
2023-02-10 02:20:38 +0900

    navbar 로그아웃 수정

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import Link from "next/link";
 import Image from "next/image";
 import { Navigaiter } from "../styles/GlobalStyle";
 
 export default function Navbar() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <Navigaiter>
+        <Link href="/">
+          <Image
+            src="/images/logo.svg"
+            width={234}
+            height={60}
+            alt="logo"
+            style={{ alignItems: "center" }}
+          />
+        </Link>
+
+        <a onClick={() => signOut()}>
+          <Link href="/">로그아웃</Link>
+        </a>
+      </Navigaiter>
+    );
+  }
+
   return (
     <Navigaiter>
       <Link href="/">
@@ -15,6 +38,7 @@ export default function Navbar() {
           style={{ alignItems: "center" }}
         />
       </Link>
+
       <Link href="/signin">로그인</Link>
     </Navigaiter>
   );
