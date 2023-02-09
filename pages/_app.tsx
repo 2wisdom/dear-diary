57d49d8c1754f66c72fb8824a7d268f6c112c6c1
Lizzie <leejihye7117@gmail.com>
2023-02-09 23:46:13 +0900

    session provider 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,24 +1,16 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 import Layout from "../components/Layout";
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
-  const [queryClient] = React.useState(() => new QueryClient());
-
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <SessionProvider session={session}>
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
-        </SessionProvider>
-      </QueryClientProvider>
+      <SessionProvider session={pageProps.session}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </SessionProvider>
     </>
   );
 }
