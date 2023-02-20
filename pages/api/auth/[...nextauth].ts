b851139b7a69a7ff7e1d3f57b2c09322ee193e77
Lizzie <leejihye7117@gmail.com>
2023-02-21 00:19:25 +0900

    prisma kakao adapter 추가

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,14 @@
 import NextAuth from "next-auth";
 import KakaoProvider from "next-auth/providers/kakao";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "../../../lib/prismadb";
 
-export const authOptions = {
+export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID!,
       clientSecret: process.env.KAKAO_CLIENT_SECRET!,
     }),
   ],
-};
-export default NextAuth(authOptions);
+});
