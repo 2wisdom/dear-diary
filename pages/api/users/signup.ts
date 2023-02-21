71a309e00b49c5cd41255350f4db2b92c738abbb
Lizzie <leejihye7117@gmail.com>
2023-02-21 15:50:28 +0900

    signup import prisma 이름 변경

--- pages/api/users/signup.ts
+++ pages/api/users/signup.ts
@@ -1,6 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import nc from "next-connect";
-import prisma from "../../../lib/prisma";
+import prisma from "../../../lib/prismadb";
 
 type User = {
   name: string;
