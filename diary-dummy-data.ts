3e36a306ac731bef3fc3c946742c08f562936d3b
Lizzie <leejihye7117@gmail.com>
2023-02-22 18:00:04 +0900

    dummy data date 수정

--- diary-dummy-data.ts
+++ diary-dummy-data.ts
@@ -6,7 +6,7 @@ export const diaryData = {
   image: { DiaryImageExample },
   content:
     "오늘은 프로젝트를 엎어버렸다. 캘린더는 디자인이 많이 필요해서 일단 그냥 일기장을 만들어야지 ! 회원가입 로그인 구현 너무 어렵다 ... 세상에나 마상에나 ... 난 병신이야 ...",
-  date: "2023.01.31",
+  date: "2023-01-31",
   author: {
     id: "asdf123",
     name: "Lizzie",
