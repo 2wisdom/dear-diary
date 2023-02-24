ca0c0c4081187a2c77457f669c24361f251487f2
Lizzie <leejihye7117@gmail.com>
2023-02-25 01:32:16 +0900

    create-diary api 연동 중

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,31 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "react-query";
 import Layout from "../components/Layout";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
+// Create a client
+const queryClient = new QueryClient();
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <SessionProvider session={pageProps.session}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </SessionProvider>
+      <QueryClientProvider client={queryClient}>
+        <SessionProvider session={pageProps.session}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </SessionProvider>
+      </QueryClientProvider>
     </>
   );
 }
