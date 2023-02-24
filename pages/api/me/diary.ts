6ac26f8cc6b0d925fc7d8c46ee72d6d91d7ef1c8
Kuby (커비!) <kuby@cocho.io>
2023-02-24 13:42:42 +0900

    gh-17 (#18)

---
+++ pages/api/me/diary.ts
@@ -0,0 +1,76 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import nc from "next-connect";
+import prisma from "../../../lib/prismadb";
+import { getServerSession } from "../../../lib/getServerSession";
+
+const handler = nc<NextApiRequest, NextApiResponse>();
+
+handler.get(async (req, res) => {
+  const session = await getServerSession(req, res);
+
+  if (!session?.id) {
+    return res.status(401).send({
+      message: "Authentication Required",
+    });
+  }
+
+  const query = req.query as {
+    take: string;
+    nextToken?: string;
+  };
+
+  const take = Number(query.take) || 20;
+
+  const list = await prisma.diary.findMany({
+    take: take + 1,
+    skip: 0,
+    ...(query.nextToken && {
+      cursor: {
+        id: query.nextToken,
+      },
+    }),
+    select: {
+      id: true,
+      content: true,
+      createdAt: true,
+      diaryDate: true,
+      image: true,
+      title: true,
+      updatedAt: true,
+    },
+    where: {
+      AND: [
+        {
+          userId: session.id,
+        },
+      ],
+    },
+    orderBy: {
+      diaryDate: "desc",
+    },
+  });
+
+  const content = list.filter((_: any, i: number) => i < take);
+
+  const next = list.find((_, i) => i === take);
+
+  const totalElements = await prisma.diary.count({
+    where: {
+      AND: [
+        {
+          userId: session.id,
+        },
+      ],
+    },
+  });
+
+  return res.send({
+    meta: {
+      nextToken: next?.id || null,
+      totalElements,
+    },
+    content,
+  });
+});
+
+export default handler;
