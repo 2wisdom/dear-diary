adc44f38384d2abee95b87d519f61914f97bd280
Lizzie <leejihye7117@gmail.com>
2023-02-27 20:50:57 +0900

    예비링크 추가(삭제예정)

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 import React from "react";
 
 export default function User() {
   return (
     <>
       <h1>User Page</h1>
+      <Link href="/create-diary">오늘 일기 쓰러가기</Link>
+      <br />
+      <Link href="/diary">일기 읽으러 가기</Link>
     </>
   );
 }
