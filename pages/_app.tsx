a0a682e0e1271df7b483e65607bf10762dd42753
Lizzie <leejihye7117@gmail.com>
2023-03-02 22:11:22 +0900

    react-query v4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,20 @@
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "react-query";
 import Layout from "../components/Layout";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 config.autoAddCss = false;
 
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
