0cd612f9cc638248aa4d2011db5885c66fbd8e8d
Lizzie <leejihye7117@gmail.com>
2023-03-04 13:22:44 +0900

    queryClient 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,37 @@
+import React from "react";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
-import React from "react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
 import Layout from "../components/Layout";
 import { config } from "@fortawesome/fontawesome-svg-core";
 import "@fortawesome/fontawesome-svg-core/styles.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 config.autoAddCss = false;
 
-// Create a client
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-
 export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = React.useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      })
+  );
+
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
         <SessionProvider session={pageProps.session}>
           <Layout>
             <Component {...pageProps} />
           </Layout>
         </SessionProvider>
-      </QueryClientProvider>
-    </>
+      </Hydrate>
+    </QueryClientProvider>
   );
 }
